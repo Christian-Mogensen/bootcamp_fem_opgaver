1cbb99b524ca8bc0f3a7f0f4ee28930bae9b5eca
Christian-Mogensen <U2407192@rts-365.dk>
2021-08-23 15:12:13 +0200

    Add comment to commited solution 03n1

--- Opgaver/Niveau1/03/index.js
+++ Opgaver/Niveau1/03/index.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+/* Den foerste linje laver en nodelist af alle elementer med class .box. 
+derefter er der en foreach loekke, som tilfoejer eventlistener
+til hver enkelte item.
+Eventlisteneren er et click event, som starter en funktion der giver
+elementet style.display = 'none' */
   document.querySelectorAll(".box").forEach((item) => {
     item.addEventListener("click", (cssSelector) => {
       item.style.display = "none";
     });
   });
-  
+
 });
