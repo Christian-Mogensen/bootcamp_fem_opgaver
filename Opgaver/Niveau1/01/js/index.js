721ac79d7f88c16cb0d18fd7e9c6a177f9895632
Christian-Mogensen <U2407192@rts-365.dk>
2021-08-23 10:41:43 +0200

    Add comment describing the stages of the solution

--- Opgaver/Niveau1/01/js/index.js
+++ Opgaver/Niveau1/01/js/index.js
@@ -1,13 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
+// variable der tager fat i knappen i DOM'en
   let btn = document.querySelector("#my-button");
+// array hvor hex-koderne er gemt
   let color = ["#7f7", "#eee"];
+// index nulstilles
   let colorCount = 0;
-  btn.addEventListener("click", () =>{
+
+// vi tilfoejer en eventlistener, som er et click-event, som kalder en anonym function
+  btn.addEventListener("click", () => {
+// index bliver opdateret med 1
     colorCount++;
-    if(colorCount ===  color.length){
-        colorCount = 0;
+// betingelse, som kigger index og om hvorvidt det er 0 eller 1
+    if (colorCount === color.length) {
+// hvis index er mere end 0, nulstil den
+      colorCount = 0;
     }
+// Vi opdatere baggrundsfarven, og goer den til array placeringens hex-kode
     btn.style.background = color[colorCount];
   });
-
 });
